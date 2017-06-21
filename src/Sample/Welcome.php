e1c39d3d19ab930762509cccfbc0c960faaa1edf
rizkyramadhan <kamil.insan@outlook.com>
2017-06-21 10:31:46 +0700

    unding Welcome page

--- src/Sample/Welcome.php
+++ src/Sample/Welcome.php
@@ -4,17 +4,6 @@ namespace Pages;
 
 class Welcome extends \Yard\Page
 {
-    public function includeJS() {
-        return [
-            'https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js',
-            'yard:biasa.js'];
-    }
-    
-    public function js() {
-return <<<JS
-    $("body").css('background', 'red');       
-JS;
-    }
     
     public function render()
     {
