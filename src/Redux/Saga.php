6ab53a3eea51e25cc2c90a34c0b259b3187391bf
rizkyramadhan <kamil.insan@outlook.com>
2017-07-03 16:06:06 +0700

    adding includeDir

--- src/Redux/Saga.php
+++ src/Redux/Saga.php
@@ -2,6 +2,19 @@
 
 namespace Yard\Redux;
 
-abstract class Saga {
-  
+class Saga {
+    public function includeDir($dir) {
+        $rc = new \ReflectionClass($this);
+        $dir = dirname($rc->getFileName()) . DIRECTORY_SEPARATOR . 'sagas';
+        $files = glob($dir . DIRECTORY_SEPARATOR . '*.js');
+        
+        $sagas = [];
+        
+        foreach ($files as $f) {
+            $saga = explode(".js", basename($f))[0];
+            $sagas[$saga] = file_get_contents($f);
+        }
+
+        return $sagas;
+    }
 }
