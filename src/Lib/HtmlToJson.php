797620154fab1cc33cb528140bb1e316b2c4f107
Naufal <bosnaufalemail@gmail.com>
2017-08-10 11:08:46 +0700

    GOES TO JSX!

--- src/Lib/HtmlToJson.php
+++ src/Lib/HtmlToJson.php
@@ -9,11 +9,59 @@ class HtmlToJson
     private static function doConvert($base, $render)
     {
         $render = str_replace('&', '~^AND^~', $render); #replace '&' with something.
-      
+
+        $init = $render;
+
+        $replacer = [
+          // ============ TAG LEVEL =============== //
+          [
+            // Replace { ... spread }
+            "regex" => '/(<[\w\W]+?){(\s?\.\.\.([\w\W]+?\s?))}/im',
+            "replacement" => '${1}js:spread="${3}',
+          ],
+          [
+            // Replace return <If confition={expression}>expression</If>
+            "regex" => '/<If condition={([\w\W]+?)}>([\w\W]+?)<\/If>/im',
+            "replacement" => 'if (${1}) return <el>${2}</el>',
+          ],
+
+          // ============ ATTRIBUTE =============== //
+          [
+            // Replace ={ expression }
+            "regex" => '/={([\w\W]+?)}/im',
+            "replacement" => '="js:${1}"',
+          ],
+          [
+            // Replace ={ expression }
+            "regex" => '/={([\w\W]+?)}/im',
+            "replacement" => '="js:${1}"',
+          ],
+
+          // ============ OUTSIDE =============== //
+          [
+            // Replace { expression }
+            "regex" => '/{([\w\W]+?)}/im',
+            "replacement" => '<js>${1}</js>',
+          ],
+          [
+            // Replace return ( expression )
+            "regex" => '/return\s?\(([\w\W]+?)\)/im',
+            "replacement" => 'return <el>${1}</el>',
+          ],
+        ];
+
+        foreach ($replacer as $key => $item) {
+          $render = preg_replace($item["regex"], $item["replacement"], $render);
+        }
+
+        var_dump($init);
+        var_dump($render);
+        die();
+
         # parse the dom
         $fdom = \FluentDom::load($render, 'text/xml', ['libxml'=> LIBXML_COMPACT ]);
         $json = new HSerializer($base, $fdom);
-        
+
         # turn it to string again, and then un-format it
         return str_replace('~^AND^~', '&', $json->__toString()); #un-replace '&'
     }
@@ -34,7 +82,7 @@ class HtmlToJson
                     $lines[$row - 1] = substr($lines[$row - 1], 0, $col -1) . '~^LT^~'. substr($lines[$row - 1], $col);
                     $newrender = implode("\n", $lines);
                     $json = self::convert($base, $newrender, $try + 1);
-          
+
                     return str_replace('~^LT^~', '<', $json);
                     ;
                 } else {
